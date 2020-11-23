76f9bad1a1aad5fca8a6c722a07303527f669810
itizidon <dng21@binghamton.edu>
2020-11-23 13:23:54 -0500

    creating sidebar

--- client/src/components/FilterForm.js
+++ client/src/components/FilterForm.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-
 const FilterForm = ({ updateRecipes }) => {
   const [filter, setFilter] = useState({ RecipeKey: 'get all' })
 
   const [tags, setTags] = useState([])
+  // const [isGoing, setIsGoing] = useState()
 
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchingTags() {
-      const { data } = await axios.get(
-        'http://localhost:8080/getTags')
+      const { data } = await axios.get('http://localhost:8080/getTags')
       setTags(data)
     }
     fetchingTags()
@@ -19,7 +18,37 @@ const FilterForm = ({ updateRecipes }) => {
   console.log(tags)
   return (
     <form onSubmit={(event) => updateRecipes(filter)}>
-
+      {tags[0] ? (
+        <div>
+          <h6>Ethnicity</h6>
+          {tags[0].ethnicity.map((cur, inx) => {
+            return (
+              <label key={inx}>
+                {cur}
+                <input name="Ethnicity" type="checkbox" />
+              </label>
+            )
+          })}
+          <h6>Recipe</h6>
+          {tags[0].recipename.map((cur, inx) => {
+            return (
+              <label key={inx}>
+                {cur}
+                <input name="Recipename" type="checkbox" />
+              </label>
+            )
+          })}
+          <h6>Time</h6>
+          {tags[0].time.map((cur, inx) => {
+            return (
+              <label key={inx}>
+                {cur}
+                <input name="time" type="checkbox" />
+              </label>
+            )
+          })}
+        </div>
+      ) : null}
     </form>
   )
 }
