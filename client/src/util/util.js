937b25706ac33a5c0f58bf3cd7303fef39387ee5
itizidon <dng21@binghamton.edu>
2020-11-30 14:16:05 -0500

    created request struct

---
+++ client/src/util/util.js
@@ -0,0 +1,7 @@
+export default function requestData (recipeInfo){
+  this.Recipename = recipeInfo.Recipename
+  this.Ethnicity = recipeInfo.Ethnicity
+  this.Method = recipeInfo.Method
+  this.Time = recipeInfo.Time
+}
+
