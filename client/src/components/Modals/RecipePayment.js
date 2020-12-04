df7b8758496356cf72e4ee71c9e4c594e510a4c1
itizidon <dng21@binghamton.edu>
2020-12-04 15:45:27 -0500

    created payment page

---
+++ client/src/components/Modals/RecipePayment.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <button onClick={()=>handleClose()}>close</button>
+      </section>
+    </div>
+  );
+};
--- client/src/components/SingleRecipe.js
+++ client/src/components/SingleRecipe.js
@@ -1,11 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import { withRouter } from 'react-router'
+import { Modal } from './Modals/RecipePayment'
 
-const SingleRecipe = (props) =>{
+const SingleRecipe = (props) => {
+  const [show, setShow] = useState(false)
   console.log(props)
-  return(
+
+  const handleClose = () => {
+    setShow(false)
+  }
+
+  return (
     <div>
-      Single Recipe
+      <Modal show={show} handleClose={handleClose}>
+        <input
+          type="tel"
+          inputmode="numeric"
+          pattern="[0-9\s]{13,19}"
+          autocomplete="cc-number"
+          maxlength="19"
+          placeholder="xxxx xxxx xxxx xxxx"
+        ></input>
+      </Modal>
+      <button
+        type="button"
+        onClick={() => {
+          setShow(true)
+        }}
+      >
+        Order Recipe
+      </button>
+      <p1>Reviews</p1>
     </div>
   )
 }
